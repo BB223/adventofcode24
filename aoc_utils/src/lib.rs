8fa7ce0eeec5580faf61b64bc18a3b65d8846c22
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-02 22:34:52 +0100

    aoc_utils

---
+++ aoc_utils/src/lib.rs
@@ -0,0 +1,32 @@
+use std::{env, fs, path::Path};
+
+use reqwest::Result;
+
+async fn get_input_web(day: u32) -> Result<String> {
+    let client = reqwest::Client::new();
+    let url = format!("https://adventofcode.com/2024/day/{}/input", day);
+    println!("Requesting... {}", url);
+
+    client
+        .get(url)
+        .header("cookie", env::var("SESSION_COOKIE").unwrap())
+        .send()
+        .await?
+        .text()
+        .await
+}
+
+pub async fn get_input(day: u32) -> String {
+    let path_str = env::var("AOC_PATH").unwrap_or("../aocfiles/".to_string()) + &day.to_string();
+    let path = Path::new(&path_str);
+    if path.exists() {
+        println!("Reading File... {}", path_str);
+        fs::read_to_string(path).unwrap()
+    } else {
+        let input = get_input_web(day).await.unwrap();
+        println!("Write File... {}", path_str);
+        fs::create_dir_all(path.parent().unwrap()).unwrap();
+        fs::write(path, input.clone()).unwrap();
+        input
+    }
+}
