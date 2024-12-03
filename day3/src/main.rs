4187aabfddb93f8597828b7f57ff363ecdab32bd
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-03 19:52:25 +0100

    day3.2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,8 +7,21 @@ async fn main() {
 
     let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
+    let mut splits = input.split("don't()");
+
+    let mut inp = splits.next().unwrap().to_owned();
+
+    for i in splits {
+        let mut dos = i.split("do()");
+        dos.next();
+        inp.push_str(&dos.fold(String::new(), |mut acc, x| {
+            acc.push_str(x);
+            acc
+        }))
+    }
+
     let rs: i32 = regex
-        .captures_iter(&input)
+        .captures_iter(&inp)
         .map(|m| {
             m.get(1).unwrap().as_str().parse::<i32>().unwrap()
                 * m.get(2).unwrap().as_str().parse::<i32>().unwrap()
