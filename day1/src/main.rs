6f3681316a244714ad5872b8319908dd051c7362
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-03 15:05:17 +0100

    day1.2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,7 +6,6 @@ async fn main() {
     let lines = input.lines();
     let mut llist: Vec<u32> = Vec::new();
     let mut rlist: Vec<u32> = Vec::new();
-    let mut sum: u32 = 0;
 
     for line in lines {
         let mut nums = line.split_whitespace();
@@ -16,12 +15,12 @@ async fn main() {
         rlist.push(right.parse::<u32>().unwrap());
     }
 
-    llist.sort();
-    rlist.sort();
 
-    for i in 0..llist.len() {
-        sum = sum + (llist[i].abs_diff(rlist[i]));
-    }
+    let sum = llist.iter().map(|num| num * count_nums(&rlist, *num)).sum::<u32>();
 
     println!("{sum}")
+}
+
+fn count_nums(list: &Vec<u32>, num: u32) -> u32 {
+    list.iter().filter(|n| **n == num).count().try_into().unwrap()
 }
