bee14df954afab1d0c15a4382a29813fb5865f48
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-08 20:41:41 +0100

    day4.2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -16,23 +16,26 @@ async fn main() {
     let bottom_right = |i: usize| bottom(right(i));
 
     let res: usize = (0..grid.len())
-        .filter(|i| grid[*i] == 'X')
-        .map(|i| {
-            vec![
-                is_xmas(i, 'M', top_left, &grid, line_len),
-                is_xmas(i, 'M', top, &grid, line_len),
-                is_xmas(i, 'M', top_right, &grid, line_len),
-                is_xmas(i, 'M', left, &grid, line_len),
-                is_xmas(i, 'M', right, &grid, line_len),
-                is_xmas(i, 'M', bottom_left, &grid, line_len),
-                is_xmas(i, 'M', bottom, &grid, line_len),
-                is_xmas(i, 'M', bottom_right, &grid, line_len),
-            ]
-            .into_iter()
-            .filter(|x| *x)
-            .count()
+        .filter(|i| grid[*i] == 'A')
+        .filter(|i| {
+            is_xmas(*i, 'M', top_left, &grid, line_len)
+                && is_xmas(*i, 'S', bottom_right, &grid, line_len)
+                && is_xmas(*i, 'M', top_right, &grid, line_len)
+                && is_xmas(*i, 'S', bottom_left, &grid, line_len)
+                || is_xmas(*i, 'M', top_left, &grid, line_len)
+                    && is_xmas(*i, 'S', bottom_right, &grid, line_len)
+                    && is_xmas(*i, 'S', top_right, &grid, line_len)
+                    && is_xmas(*i, 'M', bottom_left, &grid, line_len)
+                || is_xmas(*i, 'S', top_left, &grid, line_len)
+                    && is_xmas(*i, 'M', bottom_right, &grid, line_len)
+                    && is_xmas(*i, 'S', top_right, &grid, line_len)
+                    && is_xmas(*i, 'M', bottom_left, &grid, line_len)
+                || is_xmas(*i, 'S', top_left, &grid, line_len)
+                    && is_xmas(*i, 'M', bottom_right, &grid, line_len)
+                    && is_xmas(*i, 'M', top_right, &grid, line_len)
+                    && is_xmas(*i, 'S', bottom_left, &grid, line_len)
         })
-        .sum();
+        .count();
 
     println!("{res}");
 }
@@ -41,12 +44,9 @@ fn is_xmas(
     index: usize,
     cha: char,
     fun: impl Fn(usize) -> usize,
-    grid: &Vec<char>,
+    grid: &[char],
     line_len: usize,
 ) -> bool {
-    if cha == '_' {
-        return true;
-    }
     let next = fun(index);
     if grid.get(next).is_none_or(|c| *c != cha) {
         return false;
@@ -57,13 +57,5 @@ fn is_xmas(
     if next % line_len == line_len - 1 && index % line_len == 0 {
         return false;
     }
-    let tu = if cha == 'M' {
-        'A'
-    } else if cha == 'A' {
-        'S'
-    } else {
-        '_'
-    };
-
-    is_xmas(next, tu, fun, grid, line_len)
+    true
 }
