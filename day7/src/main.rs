9c56efa36a1cc4eecc374c41dc4e1546d09a4504
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-14 12:22:10 +0100

    day7.1

---
+++ day7/src/main.rs
@@ -0,0 +1,34 @@
+use std::collections::VecDeque;
+
+use aoc_utils::get_input;
+
+#[tokio::main]
+async fn main() {
+    let input = get_input(7).await;
+    let mut res: Vec<u64> = vec![];
+    for line in input.lines() {
+        let mut splits: VecDeque<u64> = line
+            .split(&[' ', ':'])
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse().unwrap())
+            .collect();
+        let target = splits.pop_front().unwrap();
+        let current = splits.pop_front().unwrap();
+        if is_true(splits, current, target) {
+            res.push(target);
+        }
+    }
+    let res: u64 = res.iter().sum();
+
+    println!("{res}");
+}
+
+fn is_true(mut nums: VecDeque<u64>, current: u64, target: u64) -> bool {
+    match nums.pop_front() {
+        Some(apply) => {
+            is_true(nums.clone(), current + apply, target)
+                || is_true(nums.clone(), current * apply, target)
+        }
+        None => current == target,
+    }
+}
