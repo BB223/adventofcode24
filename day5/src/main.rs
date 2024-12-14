a6c083b7651d084f56292f2c21c2af7386163ef1
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-14 11:26:55 +0100

    day5.2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,4 @@
+
 use aoc_utils::get_input;
 
 #[tokio::main]
@@ -15,29 +16,37 @@ async fn main() {
         rules.push(nums);
     }
 
-    'outer: for line in lines {
-        let nums = line.split(',').map(|s| s.parse::<u32>().unwrap());
+    for line in lines {
+        let nums: Vec<u32> = line.split(',').map(|s| s.parse().unwrap()).collect();
 
-        for rule in &rules {
-            let first = nums.clone().position(|n| n == rule[0]);
-            if first.is_none() {
-                continue;
-            }
-            let second = nums.clone().position(|n| n == rule[1]);
-            if second.is_none() {
-                continue;
-            }
+        let nums2 = correct_order(&rules, nums.clone());
 
-            if first > second {
-                continue 'outer;
-            }
+        if nums != nums2 {
+            middles.push(nums2[nums2.len() / 2]);
         }
-
-        let nums: Vec<u32> = nums.collect();
-        middles.push(nums[nums.len() / 2]);
     }
 
     let res: u32 = middles.iter().sum();
 
     println!("{res}");
+}
+
+fn correct_order(rules: &Vec<Vec<u32>>, mut nums: Vec<u32>) -> Vec<u32> {
+    for rule in rules {
+        let first = nums.iter().position(|&n| n == rule[0]);
+        if first.is_none() {
+            continue;
+        }
+        let second = nums.iter().position(|&n| n == rule[1]);
+        if second.is_none() {
+            continue;
+        }
+
+        if first > second {
+            nums.swap(first.unwrap(), second.unwrap());
+            let nums = correct_order(rules, nums);
+            return nums;
+        }
+    }
+    nums
 }
