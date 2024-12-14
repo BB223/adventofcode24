cfa781e34c1e82c22d790b86e9088d0c6bc5934f
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-14 10:46:10 +0100

    day5.1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,11 +4,40 @@ use aoc_utils::get_input;
 async fn main() {
     let input = get_input(5).await;
     let mut lines = input.lines();
+    let mut rules: Vec<Vec<u32>> = vec![];
+    let mut middles: Vec<u32> = vec![];
 
-    while let Some(line) = lines.next() {
+    for line in lines.by_ref() {
         if line.is_empty() {
             break;
         }
-
+        let nums: Vec<u32> = line.split('|').map(|s| s.parse().unwrap()).collect();
+        rules.push(nums);
     }
+
+    'outer: for line in lines {
+        let nums = line.split(',').map(|s| s.parse::<u32>().unwrap());
+
+        for rule in &rules {
+            let first = nums.clone().position(|n| n == rule[0]);
+            if first.is_none() {
+                continue;
+            }
+            let second = nums.clone().position(|n| n == rule[1]);
+            if second.is_none() {
+                continue;
+            }
+
+            if first > second {
+                continue 'outer;
+            }
+        }
+
+        let nums: Vec<u32> = nums.collect();
+        middles.push(nums[nums.len() / 2]);
+    }
+
+    let res: u32 = middles.iter().sum();
+
+    println!("{res}");
 }
