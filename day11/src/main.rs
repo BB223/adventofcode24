c0b3356d78ed3b9ad2ceb4386e296a56294a815c
BB223 <79644542+BB223@users.noreply.github.com>
2024-12-21 10:31:51 +0100

    day11.2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,33 +1,83 @@
+use std::collections::HashMap;
+
 use aoc_utils::get_input;
 
 #[tokio::main]
 async fn main() {
     let input = get_input(11).await;
 
-    let mut stones: Vec<u64> = input
+    let stones: Vec<u64> = input
         .split_whitespace()
         .map(|s| s.parse().unwrap())
         .collect();
 
-    for _ in 0..25 {
-        let mut tmp = vec![];
-        for stone in stones {
-            if stone == 0 {
-                tmp.push(1);
-            } else if stone.to_string().len() % 2 == 0 {
-                let stone_str = stone.to_string();
-                let (first, last) = stone_str.split_at(stone_str.len() / 2);
-
-                tmp.push(first.parse().unwrap());
-                tmp.push(last.parse().unwrap());
-            } else {
-                tmp.push(stone * 2024);
-            }
-        }
-        stones = tmp;
-    }
-
-    let res = stones.len();
+    let mut cache = HashMap::with_capacity(100_000_000);
+    let res: u64 = stones
+        .iter()
+        .map(|&stone| progress(stone, &mut cache, 0))
+        .sum();
 
     println!("{res}");
+}
+
+fn progress(stone: u64, cache: &mut HashMap<String, u64>, blink: u8) -> u64 {
+    if blink == 75 {
+        return 1;
+    }
+
+    if let Some(&s) = cache.get(&(format!("{},{}", stone, blink))) {
+        return s;
+    }
+
+    let ret = if stone == 0 {
+        progress(1, cache, blink + 1)
+    } else if num_digits_and_divisor(stone).0 % 2 == 0 {
+        let (_, divisor) = num_digits_and_divisor(stone);
+        let first = stone / divisor;
+        let last = stone % divisor;
+        progress(first, cache, blink + 1) + progress(last, cache, blink + 1)
+    } else {
+        progress(stone * 2024, cache, blink + 1)
+    };
+
+    cache.insert(format!("{},{}", stone, blink), ret);
+
+    ret
+}
+
+fn num_digits_and_divisor(n: u64) -> (usize, u64) {
+    const POWERS_OF_10: [u64; 20] = [
+        1,
+        10,
+        100,
+        1_000,
+        10_000,
+        100_000,
+        1_000_000,
+        10_000_000,
+        100_000_000,
+        1_000_000_000,
+        10_000_000_000,
+        100_000_000_000,
+        1_000_000_000_000,
+        10_000_000_000_000,
+        100_000_000_000_000,
+        1_000_000_000_000_000,
+        10_000_000_000_000_000,
+        100_000_000_000_000_000,
+        1_000_000_000_000_000_000,
+        10_000_000_000_000_000_000,
+    ];
+
+    let mut num = n;
+    let mut digits = 0;
+
+    while num >= 10 {
+        num /= 10;
+        digits += 1;
+    }
+    digits += 1;
+
+    let divisor = POWERS_OF_10[digits / 2];
+    (digits, divisor)
 }
